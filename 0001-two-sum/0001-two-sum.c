094811fe35cf388fd33290b333076a70a1eb6ec9
riccardo <riccardoventurino0@gmail.com>
2026-02-08 22:44:22 +0100

    Time: 102 ms (41.98%), Space: 8.7 MB (59.09%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.c
@@ -0,0 +1,19 @@
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int *result = malloc(2 * (sizeof(int)));//memory allocation using the size of the result
+    if (result==NULL) {//validating input
+        *returnSize = 0;
+        return NULL;
+    }
+    for (int i=0;i<numsSize;i++){
+        for (int j=i+1;j<numsSize;j++){//loops to verify every possible pair
+            if (*(nums + i) + *(nums+j) == target){
+                *result = i;
+                *(result + 1) = j;
+                *returnSize = 2;
+                return result;//inserting results in the final array and returning it
+            }
+        }
+    }
+    return NULL;
+    *returnSize = 0;
+}
